245bca1e6f7034cf13bbaecd0b00991660214e0b
mathdugre <mathdugre@pm.me>
2023-10-05 14:16:54 -0400

    [ADD] Initial info for BHG MTL announcement.

--- generator.py
+++ generator.py
@@ -1,7 +1,8 @@
-__author__ = 'akeshavan'
+__author__ = "akeshavan"
 from jinja2 import Environment, FileSystemLoader
 import os
 import simplejson as json
+
 
 def load_json(filename):
     """Load data from a json file
@@ -14,20 +15,22 @@ def load_json(filename):
     data : dict
     """
 
-    with open(filename, 'r') as fp:
+    with open(filename, "r") as fp:
         data = json.load(fp)
     return data
 
 
-files_to_generate = [{"filename": "index.html.tmpl", "location":"./"},
-                    {"filename": "css/stylish-portfolio.css.tmpl", "location": "./"}]
+files_to_generate = [
+    {"filename": "index.html.j2", "location": "./"},
+    {"filename": "css/stylish-portfolio.css.j2", "location": "./"},
+]
 
-env = Environment(loader=FileSystemLoader('./'))
+env = Environment(loader=FileSystemLoader("./"))
 info = load_json("data.json")
 
 for f in files_to_generate:
     template = env.get_template(f["filename"])
-    outfile = os.path.join(f["location"], f["filename"].replace(".tmpl",""))
+    outfile = os.path.join(f["location"], f["filename"].replace(".j2", ""))
     print("writing", outfile)
     with open(outfile, "w") as q:
         q.write(template.render(**info))
